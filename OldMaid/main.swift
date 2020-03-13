9aa9e16d1a649fa122e13ae5889bdf57ba333500
Selena Yang <syang@lcs.on.ca>
2020-03-12 20:28:12 -0400

    #3, added print() to check the status of the removePairs() function

--- OldMaid/main.swift
+++ OldMaid/main.swift
@@ -9,32 +9,52 @@
 import Foundation
 
 extension Hand {
-    // Function to check if pair exist
+    // Function to check if pair exist & remove
     func removePairs() {
         var numberOfCardsChecked = 0
+        // check every card in the deck to see if pair exist
         while numberOfCardsChecked < self.cards.count {
+            // The card selected to compare to the whole deck
             var pairingCard = 0
+            // The card that is comparing to the selected card
             var comparedCard = 0
             var stillPairing : Bool = true
             
+            // compare
             while stillPairing == true && comparedCard < self.cards.count-1 {
-                //If challenging card is greater than or less than compared card, then it moves up
+                //If challenging card is greater than or less than compared card, then it move up to compare to the next card
                 comparedCard = pairingCard + 1
                 if self.cards[pairingCard].rank.rawValue > self.cards[comparedCard].rank.rawValue ||  self.cards[pairingCard].rank.rawValue < self.cards[comparedCard].rank.rawValue{
+                    //Simple description to show the status of cards
                     print(pairingCard)
                     print(comparedCard)
                     print(self.cards[pairingCard].simpleDescription())
+                    print("VS.")
                     print(self.cards[comparedCard].simpleDescription())
+                    print("--------------------------------------")
+                    // move the comparing card up by one inorder to compare to the next card`
                     let element = self.cards[pairingCard]
                     self.cards.remove(at: pairingCard)
                     self.cards.insert(element, at: comparedCard)
+                  
+                    
                     pairingCard += 1
                     
                     //  If challenging card and compared card is the same, both get removed
                 } else {
+                    //simple description of card remove
+                    print(pairingCard)
+                    print(comparedCard)
+                    print(self.cards[pairingCard].simpleDescription())
+                    print(self.cards[comparedCard].simpleDescription())
+                    print("\(cards[pairingCard].simpleDescription()) and \(cards[comparedCard].simpleDescription()) get removed")
+                    print("--------------------------------------")
+                    //remove card
                     self.cards.remove(at: pairingCard)
                     self.cards.remove(at: comparedCard)
+                    
                     stillPairing = false
+                    
                 }
             }
             numberOfCardsChecked += 1
@@ -81,12 +101,15 @@ class OldMaid {
             self.playerHand.cards = newCards
             print("Player have \(newCards.count) cards")
         }
+         //remove pairs from player's hand
+        playerHand.removePairs()
+        
         //Computer has 25
         if let newCards = self.deck.randomlyDealOut(thisManyCards: 25){
             self.computerHand.cards = newCards
             print("Computer hanve \(newCards.count) cards")
         }
-        
+        //remove pairs frm computer's hand
         computerHand.removePairs()
         
     }
